df58bf42b06730792479429ef0bef4b3c6e91c98
weiwei <weismart1@gmail.com>
2020-08-09 13:53:31 +0800

    集成rocketmq

--- src/main/java/com/wei/usercenter/TestController.java
+++ src/main/java/com/wei/usercenter/TestController.java
@@ -1,11 +1,12 @@
 package com.wei.usercenter;
 
+
 import com.wei.usercenter.dao.user.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import javax.annotation.Resource;
+import java.util.*;
 
 @RestController
 public class TestController {
@@ -13,8 +14,44 @@ public class TestController {
      * // 作业1：课后研究一下@Resource和@Autowired的区别
      * // 面试题
      */
-    @Autowired
+    @Resource
     private  UserMapper userMapper;
+
+    public static String change(String p){
+        char [] temps = p.toCharArray();
+        List<Character> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = temps.length-1; i >=0 ; i--) {
+            if(!exit(temps[i],list)){
+                list.add(temps[i]);
+                sb.append(temps[i]);
+            }
+        }
+        return sb.toString();
+    }
+    //判断一个字符是否在数组中
+    public static boolean exit(char a,List<Character> list){
+        if (list.size() == 0 || list ==null){
+            return false;
+        }
+        for (Character c:list) {
+            if (a==c){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
 
 
 }
---
+++ src/main/java/com/wei/usercenter/dao/bonus/BonusEventLogMapper.java
@@ -0,0 +1,8 @@
+package com.wei.usercenter.dao.bonus;
+
+
+import com.wei.usercenter.domain.entity.bonus.BonusEventLog;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BonusEventLogMapper extends Mapper<BonusEventLog> {
+}
